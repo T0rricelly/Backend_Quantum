6156c83a9e8694f21d2a27d6ed45cc6484396cbf
Santiago <kevintorres1609@gmail.com>
2025-08-21 19:47:43 -0500

    Initial commit

---
+++ QuantumAplication/src/main/java/com/quantum/QuantumAplication/QuantumAplicationApplication.java
@@ -0,0 +1,13 @@
+package com.quantum.QuantumAplication;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class QuantumAplicationApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(QuantumAplicationApplication.class, args);
+	}
+
+}
---
+++ QuantumAplication/src/test/java/com/quantum/QuantumAplication/QuantumAplicationApplicationTests.java
@@ -0,0 +1,13 @@
+package com.quantum.QuantumAplication;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class QuantumAplicationApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
