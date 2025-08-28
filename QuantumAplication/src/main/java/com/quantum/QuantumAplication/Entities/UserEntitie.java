ea1c28e37e1dc85213bf720cfd84f4709e2d375f
Santiago <kevintorres1609@gmail.com>
2025-08-27 22:23:22 -0500

    agregando la entidad usuario

---
+++ QuantumAplication/src/main/java/com/quantum/QuantumAplication/Entities/UserEntitie.java
@@ -0,0 +1,10 @@
+package com.quantum.QuantumAplication.Entities;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+
+@Entity
+public class UserEntitie {
+    @Id
+    private long id;
+}
